85eacf1c50c61e2a5c2008ee32ccd2a3491f07f6
Eyüp Can Yönter <oniraca.om00@gmail.com>
2024-02-19 23:39:23 +0300

    create req type

---
+++ new/src/components/request/createRequestType/Actor.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div>
+            <label for="actor">Actor Role</label>
+            <input type="text" id="actor">
+        </div>
+        <div>
+            <label for="actor-department">Actor Department</label>
+            <input type="text" id="actor-department">
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, ref, onMounted } from 'vue';
+
+    export default defineComponent({
+        name: 'Actor',
+        setup() {
+        }
+    })
+</script>
--- new/src/components/tab/TabComponent.vue
+++ new/src/components/tab/TabComponent.vue
@@ -1,60 +1,63 @@
 <template>
-    <div class="box-border p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md m-10">
-      <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-        <li class="me-2">
-          <a href="#" @click="showTalepOlustur" :class="{ 'selected-tab': activeTab === 'TalepOlustur' }" class="inline-block px-4 py-3 rounded-lg">Talep Türü Oluştur</a>
-        </li>
-        <li class="me-2">
-          <a href="#" @click="showTalepDuzenle" :class="{ 'selected-tab': activeTab === 'TalepDuzenle' }" class="inline-block px-4 py-3 rounded-lg">Talep Türü Düzenle</a>
-        </li>
-        <li class="me-2">
-          <a href="#" @click="showAktorListele" :class="{ 'selected-tab': activeTab === 'StaffListing' }" class="inline-block px-4 py-3 rounded-lg">Aktör Listele</a>
-        </li>
-      </ul>
-      <div v-if="activeTab === 'TalepOlustur'">
-        <h1>Talep Türü Oluştur</h1>
-      </div>
-      <div v-if="activeTab === 'TalepDuzenle'">
-        <h1>Talep Türü Düzenle</h1>
-      </div>
-      <div v-if="activeTab === 'StaffListing'" class="p-5"> 
-        <StaffListing />
-      </div>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue'
-  import StaffListing from '@/components/tables/StaffListing.vue'
-  
-  const activeTab = ref('TalepOlustur')
-  
-  const showTalepOlustur = () => {
-    activeTab.value = 'TalepOlustur'
-  }
-  
-  const showTalepDuzenle = () => {
-    activeTab.value = 'TalepDuzenle'
-  }
-  
-  const showAktorListele = () => {
-    activeTab.value = 'StaffListing'
-  }
-  </script>
-  
-  <style scoped>
-  .selected-tab {
-    background-color: #2563eb;
-    color: white;
-  }
-  
-  .selected-tab:hover {
-    background-color: #2563eb;
-    color: white;
-  }
+	<div class="box-border p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md m-10">
+		<ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+			<li class="me-2">
+				<a href="#" @click="showTalepOlustur" :class="{ 'selected-tab': activeTab === 'TalepOlustur' }"
+					class="inline-block px-4 py-3 rounded-lg">Talep Türü Oluştur</a>
+			</li>
+			<li class="me-2">
+				<a href="#" @click="showTalepDuzenle" :class="{ 'selected-tab': activeTab === 'TalepDuzenle' }"
+					class="inline-block px-4 py-3 rounded-lg">Talep Türü Düzenle</a>
+			</li>
+			<li class="me-2">
+				<a href="#" @click="showAktorListele" :class="{ 'selected-tab': activeTab === 'StaffListing' }"
+					class="inline-block px-4 py-3 rounded-lg">Aktör Listele</a>
+			</li>
+		</ul>
+		<div v-if="activeTab === 'TalepOlustur'">
+			<CreateNewRequestTypes />
+		</div>
+		<div v-if="activeTab === 'TalepDuzenle'">
+			<h1>Talep Türü Düzenle</h1>
+		</div>
+		<div v-if="activeTab === 'StaffListing'" class="p-5">
+			<StaffListing />
+		</div>
+	</div>
+</template>
 
-  ul li:not(:last-child) a:hover {
-    color: #4b5563;
-  }
-  </style>
-  
+<script setup lang="ts">
+import { ref } from 'vue'
+import StaffListing from '@/components/tables/StaffListing.vue'
+import CreateNewRequestTypes from '@/components/request/createRequestType/CreateNewRequestTypes.vue'
+
+const activeTab = ref('TalepOlustur')
+
+const showTalepOlustur = () => {
+	activeTab.value = 'TalepOlustur'
+}
+
+const showTalepDuzenle = () => {
+	activeTab.value = 'TalepDuzenle'
+}
+
+const showAktorListele = () => {
+	activeTab.value = 'StaffListing'
+}
+</script>
+
+<style scoped>
+.selected-tab {
+	background-color: #2563eb;
+	color: white;
+}
+
+.selected-tab:hover {
+	background-color: #2563eb;
+	color: white;
+}
+
+ul li:not(:last-child) a:hover {
+	color: #4b5563;
+}
+</style>
