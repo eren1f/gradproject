9019553d1b8c113ca52f3ed0b1bccc2995211ce2
poxju <veziroglue@gmail.com>
2024-03-10 18:38:37 +0300

    eeo

--- new/src/components/topbar/Topbar.vue
+++ new/src/components/topbar/Topbar.vue
@@ -1,7 +1,29 @@
 <template>
-    <div class="fixed top-0 left-0 w-full h-[3%] bg-gray-800 z-50">
-        <button class="absolute right-0" @click="handleClick">
-            <span class="material-symbols-outlined text-red-500">
+    <div class="fixed top-0 left-0 w-full h-[5%] bg-gray-800 z-50 flex items-center">
+        <span class="material-symbols-outlined ml-[1%] text-white" style="font-size: 280%">account_circle</span>
+        <div class="flex flex-col ml-[1%]" style="font-size: 50%">
+            <div class="flex flex-row">
+            <p class="font-bold m-[2%] text-white">{{ user.firstname }}</p>
+            <p class="font-bold m-[2%] text-white">{{ user.lastname }}</p>
+            </div>
+            <p class="font-thin m-[2%] text-white">{{ user.role }}</p>
+        </div>
+        <h6 class="absolute left-0 ml-[47%] font-extrabold text-white" style="font-size: 150%;">EDUFLOW</h6>
+        <button class="absolute right-[1%] mr-[6%] xl:mr-[3%] lg:mr-[4%] md:mr-[4%]" @click="showNotifications = !showNotifications">
+            <span class="material-symbols-outlined text-white" style="font-size: 250%;">
+            notifications
+            </span>
+            <div v-if="showNotifications" class="absolute right-0 w-96 h-30 overflow-y-scroll  dark:bg-gray-800 bg-light-gray-500 rounded-xl shadow-2xl mt-[30%]">
+                <!-- Replace this with your notification content -->
+                <p class="font-bold m-[2%] text-white ">Bildirimler</p>
+                <hr class="my-2">
+                <div v-for="notification in notifications" :key="notification.id">
+                    <p class="m-[2%] text-white break-words">{{ notification.content }}</p>
+                </div>
+            </div>
+        </button>
+        <button class="absolute right-0 mr-[1%] text-red-600" @click="handleClick">
+            <span class="material-symbols-outlined" style="font-size: 250%;">
                 logout
             </span>
         </button>
@@ -24,19 +46,25 @@ export default defineComponent({
                 router.push('/');
         };
 
+        const showNotifications = ref(false);
+        const notifications = ref([
+            // Add your notifications here
+            { id: 1, content: 'OgrenciIDden yeni bir talebiniz var.'},
+            { id: 2, content: 'Notification 2' },
+            // ...
+        ]);
+        const user = ref(
+            {firstname: 'Erdem', 
+            lastname: 'Veziroglu', 
+            role: 'Ogrenci'}
+        );
+
         return {
-            handleClick
+            handleClick,
+            showNotifications,
+            notifications,
+            user
         };
     }
 });
 </script>
-
-<style scoped>
-.material-symbols-outlined {
-    font-variation-settings:
-    'FILL' 0,
-    'wght' 400,
-    'GRAD' 0,
-    'opsz' 24
-}
-</style>
