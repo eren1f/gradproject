f17aaa1a661110855eaabd6c556bc9a83923664d
eren1f <surmelisafaeren@gmail.com>
2024-03-19 21:49:29 +0300

    popup değişiklik

--- new/src/components/popup/AdvisorPopup.vue
+++ new/src/components/popup/AdvisorPopup.vue
@@ -5,8 +5,8 @@
     </button>
     <div v-if="popupVisible" class="fixed inset-0 flex justify-center items-center z-50">
       <div class="absolute inset-0 bg-gray-800 opacity-50"></div>
-      <div class="relative bg-white rounded-lg shadow-xl p-4 w-1/2 h-96">
-        <h2 class="text-lg font-bold mb-2">Talep Detayları</h2> <!-- Title -->
+      <div class="relative bg-white rounded-lg shadow-xl p-4 w-3/4 h-3/5">
+        <h2 class="text-lg font-bold mb-2 text-center">Talep Detayları</h2>
         <div class="border-b border-gray-300 mb-2"></div> <!-- Line separator -->
         <div class="overflow-y-scroll h-24 pr-4">
           <template v-if="showEditStaffModal">
@@ -24,11 +24,17 @@
         </div>
         <div class="border-b border-gray-300 mb-2"></div> <!-- Line separator -->
         <div class="flex flex-1">
-          <textarea class="border rounded border-gray-300 w-full h-24 resize-none m-1" readonly>Talep İle İlgili Notlar</textarea>
-          <textarea placeholder="Talep Açıklaması Giriniz" class="border rounded border-gray-300 w-full h-24 resize-none m-1"></textarea>
+          <div class="flex flex-col w-full">
+            <label class="font-bold mb-1">Talep İle İlgili Notlar</label>
+            <textarea class="border rounded border-gray-300 w-full h-24 resize-none m-1" readonly> </textarea>
+          </div>
+          <div class="flex flex-col w-full ml-2">
+            <label class="font-bold mb-1">Talep Açıklaması Giriniz</label>
+            <textarea class="border rounded border-gray-300 w-full h-24 resize-none m-1"></textarea>
+          </div>
         </div>
         <div class="flex justify-between mt-4">
-          <button class="bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded w-full">İptal</button>
+          <button  @click="togglePopup" class="bg-blue-500 text-white font-bold py-2 px-4 rounded w-full">İptal</button>
           <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-full mx-2">Reddet</button>
           <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full">Onayla</button>
         </div>
@@ -42,7 +48,11 @@
 export default {
   data() {
     return {
-      popupVisible: false
+      popupVisible: false,
+      showEditStaffModal: false,
+      student: {
+        id: ""
+      }
     };
   },
   methods: {
