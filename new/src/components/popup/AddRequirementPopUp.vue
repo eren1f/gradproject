c3ae50a3230a779a5415d12adfe133367952efdb
magurow <ozandede01@gmail.com>
2024-03-21 05:17:24 +0300

    delete's work now

---
+++ new/src/components/popup/AddRequirementPopUp.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+      <button @click="togglePopup" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Open Popup
+      </button>
+      <div v-if="popupVisible" class="fixed inset-0 flex justify-center items-center z-50">
+        <div class="absolute inset-0 bg-gray-800 opacity-50"></div>
+        <div class="relative bg-white rounded-lg shadow-xl p-4 w-3/4 h-3/5">
+          <h2 class="text-lg font-bold mb-2 text-center">Talep Detayları</h2>
+          <div class="border-b border-gray-300 mb-2"></div> <!-- Line separator -->
+          <div class="overflow-y-scroll h-24 pr-4">
+            <template v-if="showEditStaffModal">
+              <input v-model="id" type="text" class="p-1 border rounded">
+            </template>
+            <template v-else>
+              <div>
+                <h1 class="font-bold">Talep Gereksinimleri</h1>
+                <p>Who killed captain alex ?</p>
+                <p>dummy text</p>
+                <p>dummy text</p>
+                <p>dummy text</p>
+              </div>
+            </template>
+          </div>
+          <div class="border-b border-gray-300 mb-2"></div> <!-- Line separator -->
+          <div class="flex flex-1">
+            <div class="flex flex-col w-full">
+              <label class="font-bold mb-1">Talep İle İlgili Notlar</label>
+              <textarea class="border rounded border-gray-300 w-full h-24 resize-none m-1" readonly> </textarea>
+            </div>
+            <div class="flex flex-col w-full ml-2">
+              <label class="font-bold mb-1">Talep Açıklaması Giriniz</label>
+              <textarea class="border rounded border-gray-300 w-full h-24 resize-none m-1"></textarea>
+            </div>
+          </div>
+          <div class="flex justify-between mt-4">
+            <button  @click="togglePopup" class="bg-blue-500 text-white font-bold py-2 px-4 rounded w-full">İptal</button>
+            <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-full mx-2">Reddet</button>
+            <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full">Onayla</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script>
+  export default {
+    data() {
+      return {
+        popupVisible: false,
+        showEditStaffModal: false,
+        
+        id: ""
+        
+      };
+    },
+    methods: {
+      togglePopup() {
+        this.popupVisible = !this.popupVisible;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
--- new/src/components/request/editRequestType/EditRequestType.vue
+++ new/src/components/request/editRequestType/EditRequestType.vue
@@ -1,103 +1,107 @@
 <template>
   <div>
-      <div>
+      <div class="w-1/2 flex justify-between space-x-20">
           <div v-if="!edit">
           <RequestCredentials :selectedDepartment ="selectedDepartment"
           @update:selectedDepartment="handleDepartmentChange"/>
-          <RequestName />
+          
           </div>
       </div>
-<!--       <div v-for="request in requestTypes" :key="request.getId()">
-        <div >{{ request.getName() }}</div>
-      </div> -->
-      
+
+      <div v-if="requestTypes.length && !edit"class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                  <thead class="bg-gray-50">
+                    <tr>
+                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        ID
+                      </th>
+                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Talep Ismi
+                      </th>
+                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody class="bg-white divide-y divide-gray-200">
+                    <tr class="table-tr" v-for="request in requestTypes" :key="request.getId()" :data-id="request.getId()">
+                        <td class="px-6 py-4 whitespace-nowrap">
+                          {{ request.getId() }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                          {{ request.getName() }}
+                        </td>
+                        <td class="pr-6 py-4 whitespace-nowrap text-right text-sm font-medium">  
+                            <button @click=" editRequest(request.getId(), request.getName())" class="text-indigo-600 hover:text-indigo-900">Duzenle&nbsp;&nbsp;&nbsp;</button>
+                            <button class="text-red-600 hover:text-red-900" @click="deleteRequestType(request.getId())"> Sil</button> <!---->                         
+                        </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+      </div>
+
       <div v-if="edit" >
         <div class=" p-8 rounded-lg">
-          <h1 class="text-2xl font-bold mb-5 text-white">Talep Türünü Düzenle</h1>
-          <div class="mb-4 flex items-center">
-            <label for="Talep İsmi" class="mr-2 block text-sm font-medium text-white"> Talep İsmi</label>
-            <input type="text" :placeholder="requestOnEdit!.getName()" class="mt-1 w-1/10 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-          </div>
-          <div class="mb-4 flex justify-between">
-            <div class="mb-4 ">
-              <h2 class="text-2xl font-bold mb-5 text-white">Talep Gereksinimleri</h2>
-              <div v-for="requirement in requestRequirements">
-                <div class="flex">
-                  <label for="Gereksinim" class="mr-2 block text-sm font-medium text-white">Gereksinim: {{ requirement.name }}</label>
-                  <input type="text" :placeholder="requirement.name" v-model="newRequirementName" class="mt-1 w-1/10 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                  <label for="İndex" class="mr-2 block text-sm font-medium text-white">Gereksinim Sırası: </label>
-                  <input type="number" :placeholder="requirement.index.toString()" v-model="newRequirementIndex" class="mt-1 w-1/10 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                </div>
-                <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700" @click="deleteRequirement(requirement.requestTypeId,requirement.index)">Gereksinimi Sil</button>
-                <button class="px-4 py-2 bg-green-500 text-white rounded ml-2 hover:bg-green-700" @click="updateRequirement(requirement, {requestTypeId: requirement.requestTypeId ,name: newRequirementName,index: newRequirementIndex})">Kaydet</button>
+            <h1 class="text-4xl font-bold mb-5 text-white">Talep Türünü Düzenle</h1>
+            <div class="mb-4 flex items-center">
+              <label for="Talep İsmi" class="mr-2 block text-l font-medium text-white"> Talep Ismi</label>
+              <input type="text" :placeholder="requestOnEdit.getName()" class="mt-1 w-1/10 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+            </div>
+            <div class="mb-4 flex justify-between">
+              <div class="mb-4 ">
+                <h2 class="text-4xl font-bold mb-5 text-white">Talep Gereksinimleri</h2>
+                  <div class="overflow-y-auto max-h-[202px]">  
+                      <div v-for="requirement in requestRequirements">
+                        <div class="parent-div">
+                          <div class="flex">
+                            <label for="Gereksinim" class="mr-2 block text-l font-medium text-white">Gereksinim: {{ requirement.name }}</label>
+                            <input type="text" :placeholder="requirement.name" v-model="newRequirementName" class="mt-1 w-1/10 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+                            <label for="İndex" class="mr-2 block text-l font-medium text-white">Gereksinim Sırası: </label>
+                            <input type="number" :placeholder="requirement.index.toString()" v-model="newRequirementIndex" class="mt-1 w-1/10 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+                          </div>
+                            <div class="mt-2 flex"> 
+                              <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700" @click="deleteRequirement(requirement.requestTypeId,requirement.index)">Gereksinimi Sil </button><!-- "-->
+                              <button class="px-4 py-2 bg-green-500 text-white rounded ml-2 hover:bg-green-700" >Kaydet</button> <!--@click="updateRequirement(requirement, {requestTypeId: requirement.requestTypeId ,name: newRequirementName,index: newRequirementIndex})"-->
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                <button class="mt-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" @click="">Yeni Gereksinim Ekle</button>
               </div>
-              <button class="mt-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" @click="">Yeni Gereksinim Ekle</button>
-            </div>
-            <div class="mb-4 ">
-              <h2 class="text-2xl font-bold mb-5 text-white" >Talep Aktörleri</h2>
-              <div v-for="actor in requestActors">
-                <div class="flex">
-                  <p class="mr-2 block text-sm font-medium text-white">Aktör Id: {{ actor.staffId }}, Aktör Rolü:,  </p>
-                  <label for="İndex" class="mr-2 block text-sm font-medium text-white">Actor Sırası:</label>
-                  <input type="number" :placeholder="actor.index.toString()" class="mt-1 w-1/10 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+              <div class="mb-4 ">
+                <h2 class="text-4xl font-bold mb-5 text-white" >Talep Aktörleri</h2>
+                <div class="overflow-y-auto max-h-[202px]">
+                 <div v-for="actor in requestActors"> 
+                   <div class="actorParent-div">
+                      <div class="flex">
+                        <p class="mr-2 block text-l font-medium text-white">Aktör Id: {{ actor.staffId }}, Aktör Rolü:,  </p>
+                        <label for="İndex" class="mr-2 block text-l font-medium text-white">Actor Sırası:</label>
+                        <input type="number" :placeholder="actor.index.toString()" class="mt-1 w-1/10 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+                      </div>
+                      <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700" @click="deleteActor(actor.requestTypeId,actor.staffId,actor.index)">Aktörü Talepten Sil</button> <!-- -->
+                      <button class="px-4 py-2 bg-green-500 text-white rounded ml-2 hover:bg-green-700">Kaydet</button>
+                    </div>
+                  </div>
                 </div>
-                <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700" @click="deleteActor(actor.requestTypeId,actor.staffId,actor.index)">Aktörü Talepten Sil</button>
-                <button class="px-4 py-2 bg-green-500 text-white rounded ml-2 hover:bg-green-700">Kaydet</button>
+                <button class="mt-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" @click="">Yeni Aktör Ekle</button>
               </div>
-              <button class="mt-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" @click="">Yeni Aktör Ekle</button>
             </div>
           </div>
-        </div>
-        <div class="flex justify-between p-8">
-          <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700" @click="deleteRequestType(requestOnEdit!.getId())">Talep Türünü Sil</button>
-          <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700" @click="cancel()">İptal</button>
-        </div>
+          <div class="flex justify-between p-8">
+            <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700" @click="cancel()">İptal</button><!--  -->
+          </div> 
       </div>
-
-      <div v-if="requestTypes.length && !edit" class="flex flex-col">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      ID
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Talep İsmi
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
-                  <tr v-for="request in requestTypes" :key="request.getId()">
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      {{ request.getId() }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      {{ request.getName() }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <button @click="editRequest(request.getId(),request.getName())" class="text-indigo-600 hover:text-indigo-900">Edit</button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-
 import RequestName from '@/components/request/createRequestType/RequestName.vue';
 import RequestCredentials from '../RequestCredentials.vue';
 import { watch } from 'fs';
@@ -109,71 +113,96 @@ import  type { RequestRequirement } from '@/Models/RequestRequirements';
 import type { RequestActor } from '@/Models/RequestActor';
 
 export default defineComponent({
+  name: 'EditRequestType',
   components: {
     RequestName,
     RequestCredentials
 },
 
-  data(){
-    return{
-      edit:ref(false),
-      requestOnEdit:(null as ListRequestTypes | null),
-      requestRequirements: (null as RequestRequirement[] | null),
-      requestActors: (null as RequestActor[] | null),
-      handler : new AdminRequestHandler(),
-      newRequirementName: ref(""),
-      newRequirementIndex: ref(1),
-    }
-  },
-  methods: {
-    async editRequest(id: number, name: string){
-      this.requestOnEdit = new ListRequestTypes(id,name);
-      this.edit = true;
-      this.requestRequirements = await this.handler.getRequestRequirementsByRequestTypeId(id);
-      this.requestActors = await this.handler.getRequestActorsByRequestTypeId(id);
-    },
-    cancel(){
-      this.edit = false;
-    },
-    deleteRequirement(id: number,index: number){
-      this.handler.deleteRequestRequirement(id,index);
-    },
-    deleteActor(requestId:number, staffId: number, index: number){
-      this.handler.deleteRequestActor(requestId, staffId, index);
-    },
-    deleteRequestType(id: number){
-      console.log(id);
-      this.handler.deleteRequestType(id);
-    },
-    updateRequirement(old: RequestRequirement, newReq: RequestRequirement){
-      console.log(newReq);
-      this.handler.updateRequestRequirement(old,newReq);
-    },
-    updateActor(old: RequestActor, newActor: RequestActor){
-      this.handler.updateRequestActor(old,newActor);
-    },
-    updateRequestName(id: number, name: string){
-    }
 
-  },
+
   setup() {
-
-    let requestTypes = ref<ListRequestTypes[]>([]);
-
-    const handler = new AdminRequestHandler();
-    let selectedDepartment = ref(null as ListDepartments | null);
-
-    const handleDepartmentChange = async ( newDepartment: ListDepartments | null) => {
-      selectedDepartment.value = newDepartment;
-      
-      if (selectedDepartment.value != null){
-        requestTypes.value = await handler.getRequestTypesByDepartmentId(selectedDepartment.value.getDepartmentId());
-
-        console.log(requestTypes.value);
+   const requestActors = ref<RequestActor[]>([]);
+   const requestRequirements = ref<RequestRequirement[]>([]);
+   const requestTypes = ref<ListRequestTypes[]>([]);
+   let requestOnEdit = new ListRequestTypes(1,' ');
+   const edit = ref(false);
+   const handler = new AdminRequestHandler();
+   const selectedDepartment = ref<ListDepartments>();
+   
+  
+    const deleteRequirement = (id: number,index: number) => {
+      const parentDiv = document.querySelector('.parent-div');
+      if (parentDiv) {
+        parentDiv.remove();
       }
+      handler.deleteRequestRequirement(id,index);
+   }  
+   
+   const deleteActor = (requestId:number, staffId: number, index: number) => {
+    const parentDiv = document.querySelector('.actorParent-div');
+      if (parentDiv) {
+        parentDiv.remove();
+      }
+        handler.deleteRequestActor(requestId, staffId, index);
+      }
+
+      const deleteRequestType = (id: number) => {
+        console.log(id);
+        handler.deleteRequestType(id);
+        const tr = document.querySelector(`tr[data-id="${id}"]`)
+        if (tr) {
+          tr.remove();
+        }
+        
     }
 
-    return { selectedDepartment , handleDepartmentChange, requestTypes,handler}; 
+    
+      const cancel = () => {
+      edit.value = false;
+    }
+    
+    
+   
+    const handleDepartmentChange = async ( newDepartment: ListDepartments ) => {
+        selectedDepartment.value = newDepartment;
+        
+        if (selectedDepartment.value != null){
+          requestTypes.value = await handler.getRequestTypesByDepartmentId(selectedDepartment.value.getDepartmentId());
+  
+          console.log(requestTypes.value);
+        }
+      }
+      
+      const editRequest = async (id: number, name: string) => {
+        if(id !== null){
+          requestOnEdit.setId(id); 
+          requestOnEdit.setName(name);
+          requestRequirements.value = await handler.getRequestRequirementsByRequestTypeId(id);
+          console.log(requestRequirements.value);
+       
+          requestActors.value = await handler.getRequestActorsByRequestTypeId(id);
+          console.log(requestActors.value);
+        }
+        edit.value = true; 
+    }
+      
+      
+    // Return reactive variables and methods
+    return {
+      edit,
+      selectedDepartment,
+      handleDepartmentChange,
+      deleteRequirement,
+      editRequest,
+      deleteActor,
+      deleteRequestType,
+      cancel,
+      requestRequirements,
+      requestOnEdit,
+      requestActors,
+      requestTypes
+    };
   }
 });
 </script>
